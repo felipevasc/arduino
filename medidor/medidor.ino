8b0588186ddde8fbd59e1041d02b7a8d21626abc
FELIPE VASCONCELOS DE SOUZA - GETIC <003478@int.cagece.com.br>
2019-10-30 13:25:26 -0300

    Ajustes Medidor

--- medidor/medidor.ino
+++ medidor/medidor.ino
@@ -1,39 +1,32 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
  
-const char* ssid = "fel";
-const char* password = "vasconcelos";
+const char* ssid = "Brisa";
+const char* password = "seriaprasempre";
  
 void setup () {
- 
-Serial.begin(115200);
-WiFi.begin(ssid, password);
- 
-while (WiFi.status() != WL_CONNECTED) {
-  delay(1000);
-  Serial.print("Connecting..");
-}
- 
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print("Connecting..");
+  }
 }
  
 void loop() {
- 
-if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
- 
-HTTPClient http;  //Declare an object of class HTTPClient
- 
-http.begin("http://jsonplaceholder.typicode.com/users/1");  //Specify request destination
-int httpCode = http.GET();                                                                  //Send the request
- 
-if (httpCode > 0) { //Check the returning code
- 
-String payload = http.getString();   //Get the request response payload
-Serial.println(payload);                     //Print the response payload
- 
-}
- 
-http.end();   
-}
- 
-delay(3000);    
+  if (WiFi.status() == WL_CONNECTED) {
+    if (digitalRead(D1) < 1) {
+      HTTPClient http;
+      http.begin("https://sistemas.int.cagece.com.br/backup/iot_medidor/add.php");
+      int httpCode = http.GET();
+      if (httpCode > 0) {
+        String payload = http.getString();
+        Serial.println(payload);
+      }
+      http.end();   
+      delay(300);
+    }
+      
+  }
+  
 }
